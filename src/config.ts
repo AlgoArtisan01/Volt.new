86f81f4039849b8fcc35593bc8c2fe977ae09047
AlgoArtisan01 <rohit10chandra@gmail.com>
2025-09-02 12:36:19 +0530

    URL Updated

--- src/config.ts
+++ src/config.ts
@@ -10,6 +10,6 @@ export const BACKEND_URL =
   import.meta.env.VITE_BACKEND_URL || "https://volt-new-backend.vercel.app/";
 
 export const API_URL =
-  import.meta.env.VITE_BACKEND_URL || "https://volt-new-backend.vercel.app/";
+  import.meta.env.VITE_BACKEND_URL || 'https://volt-new-backend.vercel.app/';
 
 export const IS_PRODUCTION = import.meta.env.PROD;
